d1beaf3e890a4cfc58e9a263234976d408220a0b
vladt2007 <110296311+vladt2007@users.noreply.github.com>
2024-12-16 17:51:16 -0500

    Update main.py
    Put all code together in main, debugging + quality of life adjustments

--- main.py
+++ main.py
@@ -42,8 +42,66 @@ anum = dict(opentable(path + "/table1.csv"))
 
 amass = [float(x[0]) for x in opentable(path + "/table2.csv")]
 
+
+"""
+No longer needed, used for debuging purposes
+
 print(anum)
 print(amass)
 
 print(int(anum["Ne"]))
 print(amass[int(anum["Ne"])])
+"""
+
+
+def nbr_particules(element, charge, nbr_atomes):
+  # electrons
+  electrons = int(anum[element])
+
+  if int(charge) != 0:
+    electrons = (electrons - int(charge)) * nbr_atomes
+
+
+
+
+
+  # protons
+  protons = (int(anum[element])) * nbr_atomes
+
+  # neutrons
+  if amass[int(anum[element])] == -1.0:
+    return "Nombre(s) d'electrons: " + str(int(electrons))+ "\n"+ "Nombre(s) de protons: "+ str(int(protons))+"\n"+"Nombre(s) de neutrons: "+ "MASSE ATOMIQUE INDÉFINIE (NOMBRE DE NEUTRONS INCALCULABLE)"
+
+  else:
+    neutrons = ((round(amass[int(anum[element])], 0) - (int(anum[element])))) * nbr_atomes
+    return "Nombre(s) d'electrons: " + str(int(electrons))+ "\n"+ "Nombre(s) de protons: "+ str(int(protons))+"\n"+"Nombre(s) de neutrons: "+ str(int(neutrons))
+
+
+
+while True:
+  atome = input("Entrez le symbole de l'atome (premiere lettre en majuscule): ")
+  if atome in anum.keys():
+    break
+  else:
+    print("Atome inconnu")
+
+while True:
+  charge_atomique = input("Entrez la charge de l'atome (entrez le symbole - ou + avant le chiffre): " )
+  try:
+    charge_atomique = int(charge_atomique)
+    break
+
+  except ValueError:
+    print("Charge inconnu")
+
+while True:
+  nbr_atome = input('Entrez le nombre de particules (ex: si c est:  5.34 * 10**23 particules ecrivez 5.34e23): ')
+  try:
+    nbr_atome = float(nbr_atome)
+    break
+
+  except ValueError:
+    print("Nombre inconnu")
+
+
+print(nbr_particules(atome, charge_atomique,nbr_atome))
